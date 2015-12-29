76167d88b7480f773bcd34859402eb6bb023c5e5
Takashi Futada <tafutada@gmail.com>
2015-12-30 02:02:25 +0700

    init

---
+++ hello/n06HttpServ.js
@@ -0,0 +1,15 @@
+var http = require("http")
+
+// Http Server
+
+http.createServer(function(request, response) {
+    // Header
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    // Body
+    response.end('Hello World');
+
+}).listen(3000); // Listen port
+
+console.log('Running...')
+
+
